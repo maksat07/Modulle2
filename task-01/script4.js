1e8abc9173d4107daf916d3035c83fe871bc992d
Maksat Asanbekov <87502665+maksat07@users.noreply.github.com>
2022-01-27 00:43:49 +0100

    feat: add script4.js

---
+++ task-01/script4.js
@@ -0,0 +1,7 @@
+//4
+let suvenir = prompt('Suvenier');
+let bezdulish = prompt('Bezdulishka');
+let suvenir2 = suvenir*75;
+let bezdulish2 = bezdulish*112;
+
+document.write("Общий покупак: "+(suvenir2+bezdulish2)+" гр"+"<br>");
