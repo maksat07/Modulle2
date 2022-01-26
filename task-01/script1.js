cfbac4d151b62a6317dd949dc9dd51a84d064a43
Maksat Asanbekov <87502665+maksat07@users.noreply.github.com>
2022-01-27 00:42:00 +0100

    feat: add script2.js

---
+++ task-01/script1.js
@@ -0,0 +1,4 @@
+//1
+const message = "Привет Вася"
+document.write(message + "<br>")
+
---
+++ task-01/script2.js
@@ -0,0 +1,5 @@
+//2
+let simpleMessage = 'Vasya at will learn js';
+document.write(simpleMessage +"<br>"); 
+simpleMessage = "Ivan also will learn js";
+document.write(simpleMessage + "<br>");
